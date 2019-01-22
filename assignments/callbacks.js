c3e38a120cae8c13751b9297ff4f1b5eafa04c16
x-zen <jdharris.x@gmail.com>
2019-01-22 17:09:45 -0600

    - Completed 'closure.js' Challenge 1 - Completed 'closure.js' Challenge 2 - Completed 'closure.js' 'base'

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -21,28 +21,48 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+//------------------------------------------
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+function getLength(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb (arr.length)
+  }
+}
+
+getLength(items, function getLength(lengthOf) {
+  console.log(lengthOf);
+});
+/*-------------------------------------------
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+function last(arr,cb) {
+  for (let i = 0; i < arr.length; i--) {
+  cb (arr[i);
+  }
+}
+
+last(items, function(arr,i) {
+  console.log(arr[i],i);
+});
+//------------------------------------------*/
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
-
+//------------------------------------------
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
-
+//------------------------------------------
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+//-------------------------------------------
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
